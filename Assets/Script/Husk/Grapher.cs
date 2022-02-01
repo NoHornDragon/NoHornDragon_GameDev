0c8e42641a3798cfec6f5777f0b8a5abaad623ba
husk <68003176+JangHanjun@users.noreply.github.com>
2022-02-01 14:38:07 +0900

    joint 생성

---
+++ Assets/Script/Husk/Grapher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapher : MonoBehaviour
+{
+    public Camera mainCam;
+    public LineRenderer lineRenderer;
+    public DistanceJoint2D distanceJoint2D;
+    void Start()
+    {
+        distanceJoint2D.enabled = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector2 mousePoint = (Vector2)mainCam.ScreenToWorldPoint(Input.mousePosition);
+            lineRenderer.SetPosition(0, mousePoint);
+            lineRenderer.SetPosition(1, transform.position);
+            distanceJoint2D.connectedAnchor = mousePoint;
+            distanceJoint2D.enabled = true;
+            lineRenderer.enabled = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            distanceJoint2D.enabled = false;
+            lineRenderer.enabled = false;
+        }
+
+        if(distanceJoint2D.enabled)
+        {
+            lineRenderer.SetPosition(1, transform.position);
+        }
+    }
+}
