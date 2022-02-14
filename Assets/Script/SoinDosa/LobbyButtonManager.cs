6e20f4b9964265d0463b3a0690c72340a083ef14
SoinDosa <rlatjsgnl519@gmail.com>
2022-02-14 16:21:49 +0900

    UI 와이어프레임 제작

---
+++ Assets/Script/SoinDosa/LobbyButtonManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyButtonManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject historyPanel;
+    [SerializeField]
+    private GameObject settingsPanel;
+    [SerializeField]
+    private GameObject exitPanel;
+
+    private bool isOnePanelOpen = false;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && isOnePanelOpen)
+            AllPanelClose();
+        else if (Input.GetKeyDown(KeyCode.Escape) && !isOnePanelOpen)
+            ExitPanelOpen();
+    }
+    public void historyPanelOpen()
+    {
+        if (!isOnePanelOpen)
+        {
+            isOnePanelOpen = true;
+            historyPanel.SetActive(true);
+        }
+    }
+
+    public void settingsPanelOpen()
+    {
+        if (!isOnePanelOpen)
+        {
+            isOnePanelOpen = true;
+            settingsPanel.SetActive(true);
+        }
+    }
+
+    public void ExitPanelOpen()
+    {
+        if (!isOnePanelOpen)
+        {
+            isOnePanelOpen = true;
+            exitPanel.SetActive(true);
+        }
+    }
+
+    public void AllPanelClose()
+    {
+        isOnePanelOpen = false;
+
+        historyPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        exitPanel.SetActive(false);
+    }
+
+    public void GameQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
