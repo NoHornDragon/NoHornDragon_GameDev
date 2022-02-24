2da1f9fb24898d528cbfbe2e02ee58c767621f93
husk <huskgit@gmail.com>
2022-02-24 20:30:51 +0900

    merge 전

--- Assets/Script/Husk/Player/Player.cs
+++ Assets/Script/Husk/Player/Player.cs
@@ -52,7 +52,6 @@ public class Player : MonoBehaviour
         혹시나 저희 게임의 버그로 인해 이 버튼을 누르셨다면 정말 죄송합니다. 버그를 제보해주시면 감사합니다.
         아님 실수로 누르셨다면... 그건 좀 안타깝군요.
         */
-        Debug.Log("플레이어 위치 리셋");
         this.gameObject.transform.position = spwanPoint;
     }
 }
