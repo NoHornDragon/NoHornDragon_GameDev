f980eed29d2a44d47b11219142126e72700ab00e
husk <huskgit@gmail.com>
2022-03-15 21:44:49 +0900

    feat : windzone 비주얼, 물리 효과 연결
    파티클의 경우 맵 방향에 따라 수동으로 조절해야 함

--- Assets/Script/Husk/Object/WindZone/WindEffectManager.cs
+++ Assets/Script/Husk/Object/WindZone/WindEffectManager.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class WindEffectManager : MonoBehaviour
 {
-    [SerializeField] WindZone wind;
+    [SerializeField] GameObject windZone;
     [SerializeField] AreaEffector2D effector;
     [SerializeField] ParticleSystem particle;
-    
-    public float timerTime;
+
+    [Header("타이머 시간")]
+    [SerializeField] private float windTime;
+    [SerializeField] private float idleTime;
     private float timer;
-    bool nowWindy = true;
+    private bool nowWindy;
+
     [Header("이펙터 항목")]
     [SerializeField] private float windPower;
-    [Header("파티클 항목")]
-    [SerializeField] private float xSpeed;
 
     void Start()
     {
-        timer = timerTime;
+        nowWindy = false;
+        timer = idleTime;
+
+        windZone = transform.GetChild(1).gameObject;
+
+        windZone.transform.localEulerAngles = GetWindAngle(effector.forceAngle);
+        windZone.SetActive(false);
     }
 
     void Update()
@@ -28,23 +35,70 @@ public class WindEffectManager : MonoBehaviour
             return;
         }
 
-        timer = timerTime;
-        if(nowWindy)
-            StopWind();
-        else
-            StartWind();
+        timer = nowWindy ? idleTime : windTime;
+
+        if(nowWindy){
+            StartCoroutine(StopWind());
+        }
+        else{
+            StartCoroutine(StartWind());
+        }
     }
 
-    void StartWind()
+    WaitForSeconds waitTime = new WaitForSeconds(1f);
+    IEnumerator StartWind()
     {
-        // physic effect
+        windZone.SetActive(true);
+        
+        yield return waitTime;
 
+        effector.forceMagnitude = windPower;
 
         nowWindy = true;
     }
 
-    void StopWind()
+    IEnumerator StopWind()
     {
+        windZone.SetActive(false);
+        
+        yield return waitTime;
+
+        effector.forceMagnitude = 0;
+
         nowWindy = false;
+    }
+
+    Vector3 GetWindAngle(float effectorDir)
+    {
+        /*
+            effector의 방향은 0부터 시작해 90도마다 오른쪽, 위, 왼쪽, 아래로 이동합니다. 0, 90, 180, 270
+            파티클 이펙트를 주는 windZone의 경우 오른쪽, 위, 왼쪽, 아래 순으로 벡터(x, y)를 나열해 보면
+
+            - 오른쪽 : 0, 90                => 0
+            - 위 : -90, 0 or 270, 0         => 90
+            - 왼쪽 : 0, 270 or 0, -90       => 180
+            - 아래 : 90, 0                  => 270
+        */
+        Vector3 angle = Vector3.zero;
+
+        switch(effectorDir){
+            case 0 :
+                angle = new Vector3(0, 90, 0);
+                break;
+
+            case 90 :
+                angle = new Vector3(270, 0, 0);
+                break;
+
+            case 180 : 
+                angle = new Vector3(0, 270, 0);
+                break;
+
+            case 270 :
+                angle = new Vector3(90, 0, 0);
+                break;
+        }
+
+        return angle;
     }
 }
