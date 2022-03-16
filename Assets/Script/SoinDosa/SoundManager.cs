840af03737f878c0ac8288b8efdb91a4aba7e534
SoinDosa <rlatjsgnl519@gmail.com>
2022-03-16 20:43:33 +0900

    feat : BGM 페이드인,페이드아웃 기능 구현

--- Assets/Script/SoinDosa/SoundManager.cs
+++ Assets/Script/SoinDosa/SoundManager.cs
@@ -49,15 +49,78 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-    public void PlayBGM(string _name)
+    public void PlayBGM(string _name, bool _isFade) 
     {
-        foreach(var bgmSound in bgmSounds)
+        if (_isFade) // Fade와 함께 bgm 실행
         {
-            if(_name == bgmSound.name)
+            StartCoroutine(FadeInCoroutine(_name));
+        }
+        else // 즉시 bgm 실행
+        {
+            foreach (var bgmSound in bgmSounds) 
             {
+                if (_name == bgmSound.name)
+                {
+                    audioSourceBGM.clip = bgmSound.clip;
+                    audioSourceBGM.Play();
+                    return;
+                }
+            }
+            return;
+        }
+        return;
+    }
+
+    public void StopBGM(bool _isFade) // 즉시 bgm 중지
+    {
+        if (_isFade)
+        {
+            StartCoroutine(FadeOutCoroutine());
+        }
+        else
+        {
+            audioSourceBGM.Stop();
+        }
+    }
+
+    IEnumerator FadeInCoroutine(string _name)
+    {
+        if (audioSourceBGM.isPlaying)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                audioSourceBGM.volume = Mathf.Lerp(audioSourceBGM.volume, 0.0f, 0.2f);
+                yield return new WaitForSeconds(0.1f);
+            }
+            audioSourceBGM.Stop();
+            audioSourceBGM.volume = SettingsManager.instance.effectSlider.value;
+        }
+
+        foreach (var bgmSound in bgmSounds)
+        {
+            if (_name == bgmSound.name)
+            {
+                audioSourceBGM.volume = 0.0f;
                 audioSourceBGM.clip = bgmSound.clip;
                 audioSourceBGM.Play();
+                for (int i = 0; i < 20; i++)
+                {
+                    audioSourceBGM.volume = Mathf.Lerp(audioSourceBGM.volume, SettingsManager.instance.effectSlider.value, 0.1f);
+                    yield return new WaitForSeconds(0.1f);
+                }
+                audioSourceBGM.volume = SettingsManager.instance.effectSlider.value;
             }
         }
+    }
+
+    IEnumerator FadeOutCoroutine()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            audioSourceBGM.volume = Mathf.Lerp(audioSourceBGM.volume, 0.0f, 0.2f);
+            yield return new WaitForSeconds(0.1f);
+        }
+        audioSourceBGM.Stop();
+        audioSourceBGM.volume = SettingsManager.instance.effectSlider.value;
     }
 }
