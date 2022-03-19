069288ab959bf915896e267acd2a7ce64c6efb45
husk <huskgit@gmail.com>
2022-03-19 21:28:52 +0900

    feat : 세이브데이터 정보 추가 및 자동저장 기능 구현

---
+++ Assets/Script/Husk/Object/Paper/PaperSheet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PaperSheet : MonoBehaviour
+{
+    [Tooltip("종이조각의 번호이며 0번부터 시작합니다")]
+    int paperIndex;
+    void Start()
+    {
+        if(SaveData.instance.userData.paperList[paperIndex])
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("플레이어가 이 종이조각을 먹었습니다");
+        }
+    }
+
+    private void PlayerGetPaper()
+    {
+        SaveData.instance.userData.paperList[paperIndex] = true;
+
+        // 사라지는 애니메이션
+    }
+}
