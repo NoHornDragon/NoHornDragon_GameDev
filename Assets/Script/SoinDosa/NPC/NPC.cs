72af4ae292cdfa6b49bd2a5968f946f3298111d9
SoinDosa <rlatjsgnl519@gmail.com>
2022-03-23 01:47:01 +0900

    feat : NPC 대사 TextMesh로 수정, 방문 횟수에 따라 원하는 텍스트 지정 가능

--- Assets/Script/SoinDosa/NPC/NPC.cs
+++ Assets/Script/SoinDosa/NPC/NPC.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class NPCTalks
+{
+    public NPCTalks(int _count, string _talk)
+    {
+        count = _count;
+        talk = _talk;
+    }
+    public int count;
+    public string talk;
+}
 public class NPC : MonoBehaviour
 {
     [Header("플레이어 인식 거리")]
@@ -11,7 +22,10 @@ public class NPC : MonoBehaviour
     public float farDistance;
 
     [Header("그 외")]
-    public GameObject visitText; // NPC를 방문하면 출력되는 텍스트 <- 이걸 나중에 따로 매니저로 빼야함
+    public GameObject visitText; // NPC를 방문하면 출력되는 텍스트
+    [Header("방문 횟수가 이상일 경우 출력하는 텍스트")]
+    public NPCTalks[] npcTalks;
+
     private BoxCollider2D npcCol;
 
     public int visitCount; // NPC를 방문한 횟수
@@ -31,7 +45,6 @@ public class NPC : MonoBehaviour
     private void SetColSize()
     {
         npcCol.size = new Vector2(catchDistance, catchDistance);
-        visitText = GameObject.Find("Canvas").transform.Find("Visit_Text").gameObject;
     }
     private void SetFarColSize()
     {
@@ -42,8 +55,16 @@ public class NPC : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
-            visitText.GetComponent<Text>().text = "안녕하신가 여행자여! " + visitCount + "번 방문했구만!";
             visitText.SetActive(true);
+
+            for (int i = npcTalks.Length - 1; i >= 0; i--)
+            {
+                if(visitCount >= npcTalks[i].count)
+                {
+                    visitText.GetComponent<TextMesh>().text = npcTalks[i].talk;
+                    break;
+                }
+            }
         }
     }
 
