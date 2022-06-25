6d12724247a798bc48e18faaf41f6129d498daba
husk <huskgit@gmail.com>
2022-06-25 17:17:17 +0900

    feat : 명언 json 화 성공

--- Assets/Script/Husk/UI/WiseSayingTeller.cs
+++ Assets/Script/Husk/UI/WiseSayingTeller.cs
@@ -4,25 +4,72 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.IO;
+using System.Text;
+
+[System.Serializable]
+public class WiseSaying
+{
+    [Header("talk[0] = kor, talk[1] = eng")]
+    public string[] wiseSay;
+}
+
+[System.Serializable]
+public class WiaeSayingData
+{
+    public List<WiseSaying> texts;
+}
+
+
 public class WiseSayingTeller : MonoBehaviour
 {   
-    
-    [SerializeField] [TextArea]
-    List<string> wiseSayings;
+    [SerializeField]
+    private WiaeSayingData wiseSayingTexts;
+
     [SerializeField] private GameObject wiseSayingUI;
     [SerializeField] private TextMeshProUGUI wiseSayingText;
+
+    [Header("명언 파일 이름")][SerializeField]
+    private string wiseSayingFileName;
+
+
+
+    private void Awake()
+    {
+        ReadWiseFile();
+    }
+
     void Start()
     {
         wiseSayingUI.SetActive(false);
     }
 
+
+    [ContextMenu("명언 텍스트 로드")]
+    private void ReadWiseFile()
+    {
+        if (!File.Exists(Application.dataPath + "/Resources/WiseSayingTexts/" + wiseSayingFileName + ".json"))
+        {
+            Debug.Log(wiseSayingFileName + " load failed");
+        }
+        else
+        {
+            FileStream fs = new FileStream(string.Format("{0}/Resources/WiseSayingTexts/{1}.json", Application.dataPath, wiseSayingFileName), FileMode.Open);
+            byte[] data = new byte[fs.Length];
+            fs.Read(data, 0, data.Length);
+            fs.Close();
+            
+            string jsonData = Encoding.UTF8.GetString(data);
+            wiseSayingTexts = JsonUtility.FromJson<WiaeSayingData>(jsonData);
+            Debug.Log(jsonData);
+        }
+    }
+
     [ContextMenu("명언 UI ON")]
     private void ShowWiseSayingOnScreen()
     {
-
         // Have a appear tweening wiseSayingUI's OnEnable()
         // And will disappear automatically
-        wiseSayingText.text = wiseSayings[Random.Range(0, wiseSayings.Count)];
+        wiseSayingText.text = wiseSayingTexts.texts[Random.Range(0, wiseSayingTexts.texts.Count)].wiseSay[SettingsManager.instance.languageDropdown.value];
         wiseSayingUI.SetActive(true);
 
     }
