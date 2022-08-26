7e96d9c8841df2d442caedbdbb7f7b6225d65dfe
husk <huskgit@gmail.com>
2022-08-26 17:21:10 +0900

    feat : server 연결 기능 구현

---
+++ Assets/Script/Husk/Server/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject startMenu;
+    public InputField userNameField;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Debug.Log($"indtance already exist, so destroy objec!");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        startMenu.SetActive(false);
+        userNameField.interactable = false;
+        Client.instance.ConnectToServer();
+    }
+}
