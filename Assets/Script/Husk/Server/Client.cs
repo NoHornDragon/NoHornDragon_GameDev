6bb4c9c76091c3ba461140257f56ebc9be3e3aad
husk <huskgit@gmail.com>
2022-08-28 19:10:01 +0900

    feat : TCP활용, 메세지 보내기 기능 추가

--- Assets/Script/Husk/Server/Client.cs
+++ Assets/Script/Husk/Server/Client.cs
@@ -16,6 +16,9 @@ public class Client : MonoBehaviour
     public int myId = 0;
     public TCP tcp;
 
+    private delegate void PacketHandler(Packet packet);
+    private static Dictionary<int, PacketHandler> packetHandlers;
+
     private void Awake()
     {
         if(instance == null)
@@ -24,7 +27,7 @@ public class Client : MonoBehaviour
         }
         else if(instance != this)
         {
-            Debug.Log($"indtance already exist, so destroy objec!");
+            Debug.Log($"instance already exist, so destroy objec!");
             Destroy(this);
         }
     }
@@ -36,6 +39,8 @@ public class Client : MonoBehaviour
 
     public void ConnectToServer()
     {
+        InitializeClientData();
+
         tcp.Connect();
     }
 
@@ -43,6 +48,7 @@ public class Client : MonoBehaviour
     {
         public TcpClient socket;
         private NetworkStream stream;
+        private Packet receivedData;
         private byte[] receiveBuffer;
 
         public void Connect()
@@ -67,6 +73,9 @@ public class Client : MonoBehaviour
             }
 
             stream = socket.GetStream();
+
+            receivedData = new Packet();
+
             stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
         }
 
@@ -74,12 +83,91 @@ public class Client : MonoBehaviour
         {
             try
             {
+                int byteLength = stream.EndRead(result);
+                if(byteLength <= 0)
+                {
+                    // TODO : disconnect
+                    return;
+                }
 
+                byte[] data = new byte[byteLength];
+                Array.Copy(receiveBuffer, data, byteLength);
+
+                receivedData.Reset(HandleData(data));
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
             }
             catch(Exception ex)
             {
                 // TODO : disconnect
             }
         }
+
+        public void SendData(Packet packet)
+        {
+            try
+            {
+                if(socket != null)
+                {
+                    stream.BeginWrite(packet.ToArray(), 0, packet.Length(), null, null);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.Log($"Error sending data to server via TCP : {ex}");
+            }
+        }
+
+        private bool HandleData(byte[] data)
+        {
+            int packetLength = 0;
+
+            receivedData.SetBytes(data);
+
+            if(receivedData.UnreadLength() >= 4)
+            {
+                packetLength = receivedData.ReadInt();
+                if(packetLength <= 0)
+                {
+                    return true;
+                }
+            }
+
+            while(packetLength > 0 && packetLength <= receivedData.UnreadLength())
+            {
+                byte[] packetBytes = receivedData.ReadBytes(packetLength);
+                ThreadManager.ExecuteOnMainThread(() => 
+                {
+                    using (Packet packet = new Packet(packetBytes))
+                    {
+                        int packetId = packet.ReadInt();
+                        packetHandlers[packetId](packet);
+                    }
+                });
+
+                packetLength = 0;
+                if(receivedData.UnreadLength() >= 4)
+                {
+                    packetLength = receivedData.ReadInt();
+                    if(packetLength <= 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if(packetLength <= 1)
+                return true;
+            return false;
+        }
+    }
+
+    private void InitializeClientData()
+    {
+        packetHandlers = new Dictionary<int, PacketHandler>()
+        {
+            { (int)ServerPackets.welcome, ClientHandle.Welcome }
+        };
+
+        Debug.Log($"Initialized packets.");
     }
 }
