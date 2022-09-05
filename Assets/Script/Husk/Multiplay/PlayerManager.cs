5df5f44d5fc6cbfe82c3b0d5f9fa64921940de39
husk <68003176+JangHanjun@users.noreply.github.com>
2022-09-05 10:47:55 +0900

    chore : 유저 이름 표기 추가

--- Assets/Script/Husk/Multiplay/PlayerManager.cs
+++ Assets/Script/Husk/Multiplay/PlayerManager.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
     public int id;
     public string username;
     public Transform Player;
+
+    [SerializeField]
+    private TextMeshPro userNameText;
+
+    private void Start()
+    {
+        userNameText.text = username;
+    }
 }
