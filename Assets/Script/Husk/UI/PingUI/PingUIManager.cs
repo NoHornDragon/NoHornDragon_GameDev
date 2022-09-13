10848f27aec8de881cb02eb1b70f7d6c7181e1c8
husk <68003176+JangHanjun@users.noreply.github.com>
2022-09-13 19:29:04 +0900

    feat : ping ui 기능 개발
    - 마우스 휠 클릭시 핑 ui 등장
    - 버튼에 hover 되면 pingmanager의 PingIndex 변경
    - 현재는 로그로 핑 인덱스만 파악, 이후 커밋에서 이미지 띄우는 기능 구현 예정

---
+++ Assets/Script/Husk/UI/PingUI/PingUIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PingUI
+{
+    public class PingUIManager : MonoBehaviour
+    {
+        private bool mouseButtonDown;
+        [SerializeField] private int pingIndexToPopup = -1;
+        public int PingIndex { set{ pingIndexToPopup = value; } }
+        // should target to PingUI in inspector
+        [SerializeField] private GameObject pingInterface;
+        // Should target to player in inspector
+        [SerializeField] private Transform whereToPopup;
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(2))
+                SetPingUI(true);
+            if(Input.GetMouseButtonUp(2))
+                SetPingUI(false);                
+        }
+        
+        /// <summary>
+        /// Get ping index from ui element
+        /// </summary>
+        /// <param name="index">Ping icon index</param>
+        public void GetPingIndex()
+        {
+            if(pingIndexToPopup == -1)  return;
+
+            // TODO : 여기서 인덱스를 받아 whereToTaret 에 처리
+            Debug.Log($"{pingIndexToPopup}");
+            pingIndexToPopup = -1;
+        }
+
+
+        private void SetPingUI(bool isActive)
+        {
+            pingInterface.SetActive(isActive);
+
+            if(!isActive)
+                GetPingIndex();
+        }
+    }
+}
