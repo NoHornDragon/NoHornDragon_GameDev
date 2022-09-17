d918fbeb2e06badf1feae01cba2bca6b81c6a0d3
husk <huskgit@gmail.com>
2022-09-17 16:34:18 +0900

    feat : 이모지 UI 기능 추가
    - 화면에서 마우스 중앙을 클릭하면 이모지 UI 등장
    - 이후 해당 이모지로 드래그하면 인게임에 이모지 추가
        - 이 경우 내부 변수 _emojiInGameOffset를 사용해 플레이어 옆에 표시되게 조정 예정
        - 만일 이모지가 아니라면 인덱스가 -1이 되어서 이모지가 안나옴
        - 이는 유니티 UI의 Event Trigger 컴포넌트를 사용해 제작
    현재는 이모지를 Instantiate 하는 방식으로 되어 있는데 이후에는 이를 풀링하는 방식으로 변경할 예정. 이모지 난사를 위해

---
+++ Assets/Script/UI/InGameScene/EmojiUI/EmojiInGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace NHD.UI.EmojiUI
+{
+    public class EmojiInGame : MonoBehaviour
+    {
+        private void Start()
+        {
+            Destroy(this.gameObject, 12.5f);
+        }
+    }
+}
---
+++ Assets/Script/UI/InGameScene/EmojiUI/EmojiUIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NHD.UI.EmojiUI
+{
+    public class EmojiUIManager : MonoBehaviour
+    {
+        public List<EmojiInGame> _emojiList;
+
+        private bool _mouseButtonDown;
+        [SerializeField] private int _emojiIndexToPopup = -1;
+        public int _pingIndex { set{ _emojiIndexToPopup = value; } }
+        // should target to PingUI in inspector
+        [SerializeField] private GameObject _pingInterface;
+        // Should target to player in inspector
+        [SerializeField] private Transform _targetToPopup;
+        [SerializeField] private Vector2 _emojiInGameOffset;
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(2))
+                SetPingUI(true);
+            if(Input.GetMouseButtonUp(2))
+                SetPingUI(false);                
+        }
+
+        /// <summary>
+        /// Get ping index from ui element
+        /// </summary>
+        public void PopupEmoji()
+        {
+            if(_emojiIndexToPopup == -1)  return;
+
+            // TODO : 여기서 인덱스를 받아 whereToTaret 에 처리
+            Debug.Log($"{_emojiIndexToPopup}");
+
+            var emoji = Instantiate(_emojiList[_emojiIndexToPopup]);
+            emoji.transform.SetParent(_targetToPopup);
+            emoji.transform.localPosition = _emojiInGameOffset;
+
+            _emojiIndexToPopup = -1;
+        }
+
+
+        private void SetPingUI(bool isActive)
+        {
+            _pingInterface.SetActive(isActive);
+
+            if(!isActive)
+                PopupEmoji();
+        }
+    }
+}
+
