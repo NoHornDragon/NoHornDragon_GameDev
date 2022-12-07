bf6b063aecfe2b5f5fb9d5cdd902fb8bb037f5f0
SoinDosa <rlatjsgnl519@gmail.com>
2022-12-07 15:12:09 +0900

    [refactor] crab 로직 수정

--- Assets/Script/Entity/Enemy/Crab/Crab.cs
+++ Assets/Script/Entity/Enemy/Crab/Crab.cs
@@ -9,48 +9,19 @@ namespace NHD.Entity.Enemy.crab
 {
     public class Crab : SoundPlayableBase, IEnemy
     {
-        [SerializeField] private float ATTACK_DELAY_SEC = 3.0f;
         [SerializeField] private float REFLECT_POWER = 300.0f;
-        private EnemyState _state;
-        private bool _isAttackable;
         private Collider2D _collider;
-        private WaitForSeconds _attackDelay;
         private Transform _player;
 
         void Start()
         {
-            _isAttackable = true;
             _collider = GetComponent<Collider2D>();
-            _attackDelay = new WaitForSeconds(ATTACK_DELAY_SEC);
-        }
-
-        void Update()
-        {
-            CheckAttackable();
-        }
-
-        public void CheckAttackable()
-        {
-            if (_state == EnemyState.ATTACK && _isAttackable)
-            {
-                Attack();
-            }
         }
 
         public void Attack()
         {
-            _isAttackable = false;
-            StartCoroutine(AttackCoroutine());
-        }
-
-        IEnumerator AttackCoroutine()
-        {
             SoundManager._instance.PlayEFXAtPoint(_audioClips[0], transform.position);
             _player.GetComponent<PlayerCollider>().TriggerPlayerStunEvent(true, _collider.bounds.center, REFLECT_POWER);
-
-            yield return _attackDelay;
-
-            _isAttackable = true;
         }
 
         public void Move() { }
@@ -60,18 +31,8 @@ namespace NHD.Entity.Enemy.crab
             if (collision.CompareTag("Player"))
             {
                 _player = collision.transform;
-                _state = EnemyState.ATTACK;
+                Attack();
             }
         }
-
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.CompareTag("Player"))
-            {
-                _player = null;
-                _state = EnemyState.IDLE;
-            }
-        }
-
     }
 }
