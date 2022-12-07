9688ff60f721ec35184bfa61293fc3f3fae8c3be
SoinDosa <rlatjsgnl519@gmail.com>
2022-12-07 16:15:50 +0900

    [fix] Crab에게 위로 튕겨 다시 돌아가면 소리가 두번나는 문제 해결

--- Assets/Script/Entity/Enemy/Crab/Crab.cs
+++ Assets/Script/Entity/Enemy/Crab/Crab.cs
@@ -10,18 +10,31 @@ namespace NHD.Entity.Enemy.crab
     public class Crab : SoundPlayableBase, IEnemy
     {
         [SerializeField] private float REFLECT_POWER = 300.0f;
+        [SerializeField] private float ATTACK_DELAY = 5.0f;
+        private bool _isAttackable;
         private Collider2D _collider;
         private Transform _player;
+        private WaitForSeconds _attackDelay;
 
         void Start()
         {
+            _isAttackable = true;
             _collider = GetComponent<Collider2D>();
+            _attackDelay = new WaitForSeconds(ATTACK_DELAY);
         }
 
         public void Attack()
         {
             SoundManager._instance.PlayEFXAtPoint(_audioClips[0], transform.position);
             _player.GetComponent<PlayerCollider>().TriggerPlayerStunEvent(true, _collider.bounds.center, REFLECT_POWER);
+        }
+
+        IEnumerator AttackCoroutine()
+        {
+            _isAttackable = false;
+            Attack();
+            yield return _attackDelay;
+            _isAttackable = true;
         }
 
         public void Move() { }
@@ -31,7 +44,10 @@ namespace NHD.Entity.Enemy.crab
             if (collision.CompareTag("Player"))
             {
                 _player = collision.transform;
-                Attack();
+                if(_isAttackable)
+                {
+                    StartCoroutine(AttackCoroutine());
+                }
             }
         }
     }
